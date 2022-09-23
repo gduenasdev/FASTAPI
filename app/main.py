53180606989f4916f7f93c665dfc5f33a3ed131c
Gabriel Duenas <duenasdevelops@gmail.com>
2022-09-22 21:39:32 -0400

    establish routes for posts and users

--- app/main.py
+++ app/main.py
@@ -1,17 +1,12 @@
-from email import message
-from typing import Optional, List
-from fastapi import FastAPI, Response, status, HTTPException, Depends
+from fastapi import FastAPI
 from random import randrange
-from passlib.context import CryptContext
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
-from sqlalchemy.orm import Session
 import models
-import schemas
-from database import engine, get_db
+from database import engine
+from routers import post, user
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -41,81 +36,10 @@ def find_index_post(id):
         if post["id"] == id:
             return  i
 
+app.include_router(post.router)
+app.include_router(user.router)
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
-@app.get("/sqlalchemy")
-def test_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return posts
-
-@app.get("/posts", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
-    # cursor.execute("SELECT * FROM post") SQL implementation
-    # posts = cursor.fetchall()
-    posts = db.query(models.Post).all()
-    return posts
-
-@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
-    # cursor.execute("""INSERT INTO post (title, content, published) 
-    #     VALUES (%s, %s, %s) RETURNING *""", (post.title, post.content, post.published))
-    # new_post = cursor.fetchone()
-    # conn.commit() SQL implementation
-    new_post = models.Post(**post.dict()) # automatically unpacks dict
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-    return new_post
-
-@app.get("/posts/{id}", response_model=schemas.Post)
-def get_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM post WHERE id=%s""", (str(id),))
-    # post = cursor.fetchone() SQL implementation
-    post = db.query(models.Post).filter(models.Post.id == id).first() # equivalent to a WHERE
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"post with id: {id} was not found")
-    return post
-
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("""DELETE FROM post WHERE id=%s RETURNING *""", (str(id),))
-    # deleted_post = cursor.fetchone()
-    # conn.commit() SQL implementation
-    deleted_post = db.query(models.Post).filter(models.Post.id == id)
-    if deleted_post.first() == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-        detail=f"post with id: {id} does not exist")
-    deleted_post.delete(synchronize_session=False)
-    db.commit()
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-@app.put("/posts/{id}", response_model=schemas.Post)
-def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
-    # cursor.execute("""UPDATE post SET title=%s, content=%s, published=%s WHERE id=%sRETURNING *""",
-    # (post.title, post.content, post.published, str(id)))
-    # updated_post = cursor.fetchone()
-    # conn.commit() SQL implementation
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-    post = post_query.first()
-    if post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-        detail=f"post with id: {id} does not exist")
-
-    post_query.update(updated_post.dict(), synchronize_session=False)
-    db.commit()
-    return post_query.first()
-
-@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    # has the password - user.password
-    hashed_password = pwd_context.hash(user.password)
-    user.password = hashed_password
-    new_user = models.User(**user.dict()) # automatically unpacks dict
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-
-    return new_user

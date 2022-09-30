551073a06aac35390cf079d3ea08ab8e935e631b
Gabriel Duenas <duenasdevelops@gmail.com>
2022-09-30 17:01:01 -0400

    require oauth2 for creating post

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from typing import List
-import models, schemas
+from app.oauth2 import get_current_user
+import models, schemas, oauth2
 from database import get_db
 
 router = APIRouter(
@@ -18,7 +19,7 @@ def get_posts(db: Session = Depends(get_db)):
     return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""INSERT INTO post (title, content, published) 
     #     VALUES (%s, %s, %s) RETURNING *""", (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
